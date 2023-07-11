3c35068c85a5411e7f4e54f155afb674ebf207d8
Umar Wahid <66332510+umarwahid@users.noreply.github.com>
2023-07-12 00:25:21 +0500

    Add files via upload

---
+++ server.mjs
@@ -0,0 +1,20 @@
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.get('/name/:myName', (req, res) => {
+    const name = req.params.myName;
+
+    console.log("🚀 ~ file: server.mjs:12 ~ app.get ~ name:", name);
+  
+    res.send({message: `Welcome ${name} to our Site`})
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
