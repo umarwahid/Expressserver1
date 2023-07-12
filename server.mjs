f406e674559d689aa9f549d6b98eefc21aab7ac2
Umar Wahid <66332510+umarwahid@users.noreply.github.com>
2023-07-12 09:31:46 +0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! Umar Here')
 });
 
 app.get('/name/:myName', (req, res) => {
